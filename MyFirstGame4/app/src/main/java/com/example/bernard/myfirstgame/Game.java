66837d8476b53d8992641dd2e781afe02ec55995
Bernard <bcang@iastate.edu>
2016-10-03 16:11:03 -0500

    Test game with moving Background

---
+++ MyFirstGame4/app/src/main/java/com/example/bernard/myfirstgame/Game.java
@@ -0,0 +1,20 @@
+package com.example.bernard.myfirstgame;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Game extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //turn title off
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //set screen to full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(new GamePanel(this));
+    }
+}
