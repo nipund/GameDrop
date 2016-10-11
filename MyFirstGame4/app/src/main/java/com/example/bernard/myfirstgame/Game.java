5aaf2c0dbafca398df75b55b43b34575b10bc9e5
Bernard <bcang@iastate.edu>
2016-10-11 13:44:25 -0500

    Intent

--- MyFirstGame4/app/src/main/java/com/example/bernard/myfirstgame/Game.java
+++ MyFirstGame4/app/src/main/java/com/example/bernard/myfirstgame/Game.java
@@ -1,6 +1,7 @@
 package com.example.bernard.myfirstgame;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
@@ -11,6 +12,7 @@ public class Game extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
         //turn title off
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         //set screen to full screen
--- MyFirstGame4/app/src/main/java/com/example/bernard/myfirstgame/MainPage.java
+++ MyFirstGame4/app/src/main/java/com/example/bernard/myfirstgame/MainPage.java
@@ -1,6 +1,8 @@
 package com.example.bernard.myfirstgame;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,16 +10,38 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import java.io.ByteArrayOutputStream;
+
 public class MainPage extends AppCompatActivity {
 
     public ImageButton but1;
+    public ImageButton but2;
     public void init(){
         but1 = (ImageButton)findViewById(R.id.imageButton);
+        but2 = (ImageButton)findViewById(R.id.imageButton2);
         but1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent toy = new Intent(MainPage.this,Edit.class);
+                Intent toy = new Intent(MainPage.this,Game.class);
+                Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.helicopter); // your bitmap
+                ByteArrayOutputStream bs = new ByteArrayOutputStream();
+                b.compress(Bitmap.CompressFormat.PNG, 50, bs);
+                toy.putExtra("byteArray", bs.toByteArray());
+                startActivity(toy);
             }
+
+        });
+        but2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent toy = new Intent(MainPage.this,Game.class);
+                Bitmap b = BitmapFactory.decodeResource(getResources(),R.drawable.ball); // your bitmap
+                ByteArrayOutputStream bs = new ByteArrayOutputStream();
+                b.compress(Bitmap.CompressFormat.PNG, 50, bs);
+                toy.putExtra("byteArray", bs.toByteArray());
+                startActivity(toy);
+            }
+
         });
     }
 
