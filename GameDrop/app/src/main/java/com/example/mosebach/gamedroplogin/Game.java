82c71b9fb6930947bb0b354428b8022aa04f9bdc
Bernard Ang <bcang@mac-air-20.local>
2016-11-07 19:37:40 -0600

    Merge with game

---
+++ GameDrop/app/src/main/java/com/example/mosebach/gamedroplogin/Game.java
@@ -0,0 +1,29 @@
+package com.example.mosebach.gamedroplogin;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Game extends Activity {
+    int pic;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //get intent
+
+        Bundle bundle = this.getIntent().getExtras();
+        pic = bundle.getInt("image");
+
+        //turn title off
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //set screen to full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(new GamePanel(this, pic));
+    }
+}
