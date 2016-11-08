c32e71753123f38ad42b3e494bc288c08c1d3265
Nipun Dayanath <nipun@iastate.edu>
2016-11-07 19:04:47 -0600

    Added element store class

---
+++ GameDrop/app/src/main/java/ElementStore.java
@@ -0,0 +1,13 @@
+import com.example.mosebach.gamedroplogin.R;
+
+/**
+ * Created by Nipun on 11/7/2016.
+ */
+
+public class ElementStore {
+    public static int elements[] = {
+            R.drawable.basketball,
+            R.drawable.sample_element_1,
+            R.drawable.duck
+    };
+}
