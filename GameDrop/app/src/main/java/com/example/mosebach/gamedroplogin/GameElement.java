de93678b6a7c4594d406e400ce8a73d070f1f0bc
Nipun Dayanath <nipun@iastate.edu>
2016-11-07 20:31:04 -0600

    Fixed GameElement class drawable issues

--- GameDrop/app/src/main/java/com/example/mosebach/gamedroplogin/GameElement.java
+++ GameDrop/app/src/main/java/com/example/mosebach/gamedroplogin/GameElement.java
@@ -9,15 +9,15 @@ import android.graphics.drawable.Drawable;
 public class GameElement {
 
     transient Drawable pic;
-    int x, y;
+    int x, y, pic_id;
     int dx, dy;
     int width, height;
     String name;
     //tied to another element?do elements keep score or level?
 
     // Set construct
-    public GameElement(Drawable d, int x, int y, int width, int height, String name){
-        this.pic = d;
+    public GameElement(int pic_id, int x, int y, int width, int height, String name){
+        this.pic_id = pic_id;
         this.x = x;
         this.y = y;
         this.dx = 0;
@@ -26,6 +26,8 @@ public class GameElement {
         this.height = height;
         this.name = name;
     }
+
+    public void setPic(Drawable pic) {this.pic = pic;}
 
     public int getRight() {
         return x + width;
